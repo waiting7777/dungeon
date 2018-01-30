860f23e34bcec96160bbe09eb5d794221743c62a
waiting7777 <waiting0819@msn.com>
2018-01-30 14:02:30 +0800

    enemy stats

--- js/prefabs/Enemy.js
+++ js/prefabs/Enemy.js
@@ -16,7 +16,58 @@ DunCrawl.Enemy = function(state, data) {
 
     this.anchor.setTo(0.5)
 
+    //show enemy stats
+    var x = 0
+    var y = -4
+
+    //stats panel
+    var bitmapRect = this.game.add.bitmapData(28, 32)
+    bitmapRect.ctx.fillStyle = '#0000FF'
+    bitmapRect.ctx.fillRect(0, 0, 28, 32)
+
+    //sprite for the enemy stats
+    this.panel = new Phaser.Sprite(this.game, x - 2, y - 2, bitmapRect)
+    this.panel.alpha = 0.6
+    this.addChild(this.panel)
+
+    var style = {
+        font: '7px Prstart',
+        fill: '#FFF',
+        align: 'left'
+    }
+
+    //health
+    this.healthIcon = new Phaser.Sprite(this.game, x, y, 'heart')
+    this.healthIcon.scale.setTo(0.3)
+    this.addChild(this.healthIcon)
+
+    this.healthLabel = new Phaser.Text(this.game, x + 10, y, '', style)
+    this.addChild(this.healthLabel)
+
+    //attack
+    this.attackIcon = new Phaser.Sprite(this.game, x, y + 10, 'attack')
+    this.attackIcon.scale.setTo(0.35)
+    this.addChild(this.attackIcon)
+
+    this.attackLabel = new Phaser.Text(this.game, x + 10, y + 10, '', style)
+    this.addChild(this.attackLabel)
+
+    //defense
+    this.defenseIcon = new Phaser.Sprite(this.game, x, y + 20, 'shield')
+    this.defenseIcon.scale.setTo(0.2)
+    this.addChild(this.defenseIcon)
+
+    this.defenseLabel = new Phaser.Text(this.game, x + 10, y + 20, '', style)
+    this.addChild(this.defenseLabel)
+
+    this.refreshStats()
 }
 
 DunCrawl.Enemy.prototype = Object.create(Phaser.Sprite.prototype)
 DunCrawl.Enemy.prototype.constructor = DunCrawl.Unit
+
+DunCrawl.Enemy.prototype.refreshStats = function() {
+    this.healthLabel.text = Math.ceil(this.data.health)
+    this.attackLabel.text = Math.ceil(this.data.attack)
+    this.defenseLabel.text = Math.ceil(this.data.defense)
+}
